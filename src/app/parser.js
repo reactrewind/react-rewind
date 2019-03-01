c9a41f05bcb286eb64a245ef42e69be3e1c1ee8c
kiacolbert <colber16@gmail.com>
2019-03-01 16:20:55 -0500

    Created functions.

--- src/app/parser.js
+++ src/app/parser.js
@@ -3,20 +3,25 @@ const fs = require('fs');
 const estraverse = require('estraverse');
 const escodegen = require('escodegen');
 const path = require('path');
-// contains useReducer
-const file = '/node_modules/react/cjs/react.development.js';
-// 20k lines
-// const file = '/node_modules/react-dom/cjs/react-dom.development.js';
+
+// react files
+const useReducerfile = '/node_modules/react/cjs/react.development.js';
+const commitAllHostEffectsfile = '/node_modules/react-dom/cjs/react-dom.development.js';
+
+// generated file names
+const genReactDev = 'generatedReact.development.js';
+const genReactDom = 'generatedReact-dom.development.js';
 
 // convert file to string and parse
-const dir = path.join(__dirname, file);
-const fileString = fs.readFileSync(dir, { encoding: 'utf-8' });
-const ast = esprima.parseModule(fileString);
-
-
-// declare function to insert
+function parseFile(file) {
+  const dir = path.join(__dirname, file);
+  const fileString = fs.readFileSync(dir, { encoding: 'utf-8' });
+  const ast = esprima.parseModule(fileString);
+  return ast;
+}
+// declare functions to insert
 // TODO: Un-comment timeTravelTracker
-function replacer() {
+function useReducerReplacement() {
   let dispatcher = resolveDispatcher();
   function reducerWithTracker(state, action) {
     const newState = reducer(state, action);
@@ -32,20 +37,42 @@ function replacer() {
   }
   return dispatcher.useReducer(reducerWithTracker, initialArg, init);
 }
-
-const replacerNode = esprima.parseScript(replacer.toString());
+function commitAllHostEffectsReplacement() {
+  console.log('You just got swapped!');
+}
 
 // traverse ast to find method and replace body with our node's body
-estraverse.replace(ast, {
-  enter(node) {
-    if (node.type === 'FunctionDeclaration') {
-      if (node.id.name === 'useReducer') {
-        node.body = replacerNode.body[0].body;
+function traverseTree(replacementNode, functionName, ast) {
+  estraverse.replace(ast, {
+    enter(node) {
+      if (node.type === 'FunctionDeclaration') {
+        if (node.id.name === functionName) {
+          node.body = replacementNode.body[0].body;
+        }
       }
-    }
-  },
-});
-const code = escodegen.generate(ast);
-fs.writeFileSync('genCode.js', code, (err) => {
-  if (err) throw new Error(err.message);
-});
+    },
+  });
+}
+
+function generateFile(filename, ast) {
+  const code = escodegen.generate(ast);
+  fs.writeFileSync(filename, code, (err) => {
+    if (err) throw new Error(err.message);
+  });
+}
+
+const parseAndGenerate = () => {
+  const ast = parseFile(useReducerfile);
+  // const ast = parseFile(commitAllHostEffectsfile);
+
+  const injectableUseReducer = esprima.parseScript(useReducerReplacement.toString());
+  // const injectablecommitAllHostEffects = esprima.parseScript(useReducerReplacement.toString());
+
+  traverseTree(injectableUseReducer, 'useReducer', ast);
+  // traverseTree(injectablecommitAllHostEffects, 'commitAllHostEffects');
+
+  generateFile(genReactDev, ast);
+  // generateFile(genReactDom);
+};
+// parseAndGenerate();
+module.exports = parseAndGenerate;
