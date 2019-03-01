371245fde88afc3c412e81ab2aa0d19b8ce9e4a0
kiacolbert <colber16@gmail.com>
2019-03-01 16:45:13 -0500

    Add commitAllHostEffects code and functionality.

--- src/app/parser.js
+++ src/app/parser.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const estraverse = require('estraverse');
 const escodegen = require('escodegen');
 const path = require('path');
+const _ = require('lodash');
 
 // react files
 const useReducerfile = '/node_modules/react/cjs/react.development.js';
@@ -38,7 +39,101 @@ function useReducerReplacement() {
   return dispatcher.useReducer(reducerWithTracker, initialArg, init);
 }
 function commitAllHostEffectsReplacement() {
-  console.log('You just got swapped!');
+  while (nextEffect !== null) {
+    {
+      setCurrentFiber(nextEffect);
+    }
+    recordEffect();
+
+    var effectTag = nextEffect.effectTag;
+
+    if (effectTag & ContentReset) {
+      commitResetTextContent(nextEffect);
+    }
+
+    if (effectTag & Ref) {
+      var current$$1 = nextEffect.alternate;
+      if (current$$1 !== null) {
+        commitDetachRef(current$$1);
+      }
+    }
+
+    // The following switch statement is only concerned about placement,
+    // updates, and deletions. To avoid needing to add a case for every
+    // possible bitmap value, we remove the secondary effects from the
+    // effect tag and switch on that value.
+    var primaryEffectTag = effectTag & (Placement | Update | Deletion);
+    switch (primaryEffectTag) {
+      case Placement:
+        {
+          //editbyme
+          window.postMessage({
+            type: 'EFFECT',
+            data: {
+              primaryEffectTag: 'PLACEMENT',
+              effect: _.cloneDeep(nextEffect)
+            }
+          });
+          
+          commitPlacement(nextEffect);
+          // Clear the "placement" from effect tag so that we know that this is inserted, before
+          // any life-cycles like componentDidMount gets called.
+          // TODO: findDOMNode doesn't rely on this any more but isMounted
+          // does and isMounted is deprecated anyway so we should be able
+          // to kill this.
+          nextEffect.effectTag &= ~Placement;
+          break;
+        }
+      case PlacementAndUpdate:
+        {
+          // Placement
+          commitPlacement(nextEffect);
+          // Clear the "placement" from effect tag so that we know that this is inserted, before
+          // any life-cycles like componentDidMount gets called.
+          nextEffect.effectTag &= ~Placement;
+
+          // Update
+          var _current = nextEffect.alternate;
+          commitWork(_current, nextEffect);
+          break;
+        }
+      case Update:
+        {
+          //editbyme
+          window.postMessage({
+            type: 'EFFECT',
+            data: {
+              primaryEffectTag: 'UPDATE',
+              effect: _.cloneDeep(nextEffect),
+              current: _.cloneDeep(nextEffect.alternate),
+            },
+          });
+
+          var _current2 = nextEffect.alternate;
+          commitWork(_current2, nextEffect);
+          break;
+        }
+      case Deletion:
+        {
+          //editbyme
+          window.postMessage({
+            type: 'EFFECT',
+            data: {
+              primaryEffectTag: 'DELETION',
+              effect: _.cloneDeep(nextEffect),
+            },
+          });
+
+          commitDeletion(nextEffect);
+          break;
+        }
+    }
+    nextEffect = nextEffect.nextEffect;
+  }
+
+  {
+    resetCurrentFiber();
+  }
 }
 
 // traverse ast to find method and replace body with our node's body
@@ -62,17 +157,17 @@ function generateFile(filename, ast) {
 }
 
 const parseAndGenerate = () => {
-  const ast = parseFile(useReducerfile);
-  // const ast = parseFile(commitAllHostEffectsfile);
-
+  // first file
+  let ast = parseFile(useReducerfile);
   const injectableUseReducer = esprima.parseScript(useReducerReplacement.toString());
-  // const injectablecommitAllHostEffects = esprima.parseScript(useReducerReplacement.toString());
-
   traverseTree(injectableUseReducer, 'useReducer', ast);
-  // traverseTree(injectablecommitAllHostEffects, 'commitAllHostEffects');
-
   generateFile(genReactDev, ast);
-  // generateFile(genReactDom);
+  // second file
+  ast = parseFile(commitAllHostEffectsfile);
+  const injectableCommitAllHostEffects = esprima.parseScript(commitAllHostEffectsReplacement.toString());
+  traverseTree(injectableCommitAllHostEffects, 'commitAllHostEffects', ast);
+  generateFile(genReactDom, ast);
 };
+
 // parseAndGenerate();
 module.exports = parseAndGenerate;
