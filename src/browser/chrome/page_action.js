1204b7ea7aa024634373c39c8d73b0ddfd2accaf
Kia <colber16@gmail.com>
2019-03-20 18:11:11 -0400

    Pop up.

--- src/browser/chrome/page_action.js
+++ src/browser/chrome/page_action.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.querySelector('#color_btn');
+  const btn = document.querySelector('#gitHub_btn');
   btn.addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      console.log(tabs);
-      chrome.tabs.sendMessage(tabs[0].id, "change_color");  
+     chrome.tab.create({ url:'https://github.com/reactrewind/react-rewind' });
     });
+    // chrome.runtime.getURL('https://github.com/reactrewind/react-rewind');
+  // btn.addEventListener('click', () => {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  //     console.log(tabs);
+  //      chrome.tabs.sendMessage(tabs[0].id, "change_color");
+     
+  //   });
   });
 });
