a21d85aa9a8a6482406df9eb055d2292c1bf6ff0
Victor Varaschin <vvaraschin@gmail.com>
2019-03-21 19:46:54 -0400

    Removed console.log - preparing for merge to master

--- src/browser/chrome/extension.js
+++ src/browser/chrome/extension.js
@@ -8,11 +8,10 @@
     // We get an object { type: 'TIMETRAVEL', direction: 'forward' }
     window.postMessage(msg);
   });
-  console.log('inject library');
+
   window.addEventListener('message', (msg) => {
     // When our injected scripts post messages (from useReducer in 'react'),
     // we receive it here and send it to our app loaded on the DevTool.
-    if (msg.data.type === 'DISPATCH') console.log('Got msg from useReducer: ', msg.data.data);
     if (msg.data.type === 'DISPATCH') port.postMessage(msg.data.data);
   });
 }());
