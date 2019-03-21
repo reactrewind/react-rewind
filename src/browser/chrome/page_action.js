cd4aa6e4ab4ab09e5847b0c9169c31f07540f436
Kia <colber16@gmail.com>
2019-03-20 23:08:32 -0400

    Deleted links.js and moved function to page_action.js

--- src/browser/chrome/page_action.js
+++ src/browser/chrome/page_action.js
@@ -1,15 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.querySelector('#gitHub_btn');
-  btn.addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-     chrome.tab.create({ url:'https://github.com/reactrewind/react-rewind' });
-    });
-    // chrome.runtime.getURL('https://github.com/reactrewind/react-rewind');
-  // btn.addEventListener('click', () => {
-  //   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  //     console.log(tabs);
-  //      chrome.tabs.sendMessage(tabs[0].id, "change_color");
-     
-  //   });
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  var links = document.getElementsByTagName('a');
+  for (var i = 0; i < links.length; i++) {
+    (function() {
+      var ln = links[i];
+      var location = ln.href;
+      ln.onclick = function() {
+        chrome.tabs.create({active: true, url: location});
+      };
+    })();
+  }
 });
