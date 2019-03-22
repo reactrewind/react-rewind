4a40259cce1613526a945a9b77c505e4efd98ef4
Victor Varaschin <vvaraschin@gmail.com>
2019-03-22 09:31:39 -0400

    only necessaary files to launch devtool

--- src/app/styles/Details.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const DetailsWrapper = styled.div`
-  padding-left: 10px;
-  padding-top: 10px;
-  /* background-color: red; */
-  overflow: auto;
-  height: calc(100% - 45px)
-`;
