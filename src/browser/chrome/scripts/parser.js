5f6bbbe0255824668b2ffd9f2c284801d28cd492
Victor Varaschin <vvaraschin@gmail.com>
2019-03-25 22:48:00 -0400

    Changed function injection. Not changing the body of function anymore, less dependant on Reacts internal.

--- src/browser/chrome/scripts/parser.js
+++ src/browser/chrome/scripts/parser.js
@@ -3,8 +3,7 @@ const estraverse = require('estraverse');
 const escodegen = require('escodegen');
 
 // declare functions to insert
-function useReducerReplacement() {
-  const dispatcher = resolveDispatcher();
+function useReducer(reducer, initialArg, init) {
   function reducerWithTracker(state, action) {
     const newState = reducer(state, action);
     timeTravelLList.tail.value.actionDispatched = true;
@@ -18,7 +17,57 @@ function useReducerReplacement() {
     });
     return newState;
   }
-  return dispatcher.useReducer(reducerWithTracker, arguments[1], arguments[2]);
+
+  return useReducerOriginal(reducerWithTracker, initialArg, init);
+}
+
+function commitWork(current, nextEffect) {
+  if (Object.keys(funcStorage).length === 0) {
+    funcStorage.commitDeletion = commitDeletionOriginal;
+    funcStorage.commitPlacement = commitPlacementOriginal;
+    funcStorage.commitWork = commitWorkOriginal;
+    funcStorage.prepareUpdate = prepareUpdate;
+  }
+
+  timeTravelLList.append({
+    primaryEffectTag: 'UPDATE',
+    effect: _.cloneDeep(nextEffect),
+    current: _.cloneDeep(current),
+  });
+
+  return commitWorkOriginal(current, nextEffect);
+}
+
+function commitDeletion(nextEffect) {
+  if (Object.keys(funcStorage).length === 0) {
+    funcStorage.commitDeletion = commitDeletionOriginal;
+    funcStorage.commitPlacement = commitPlacementOriginal;
+    funcStorage.commitWork = commitWorkOriginal;
+    funcStorage.prepareUpdate = prepareUpdate;
+  }
+
+  timeTravelLList.append({
+    primaryEffectTag: 'DELETION',
+    effect: _.cloneDeep(nextEffect),
+  });
+  
+  return commitDeletionOriginal(nextEffect);
+}
+
+function commitPlacement(nextEffect) {
+  if (Object.keys(funcStorage).length === 0) {
+    funcStorage.commitDeletion = commitDeletionOriginal;
+    funcStorage.commitPlacement = commitPlacementOriginal;
+    funcStorage.commitWork = commitWorkOriginal;
+    funcStorage.prepareUpdate = prepareUpdate;
+  }
+
+  timeTravelLList.append({
+    primaryEffectTag: 'PLACEMENT',
+    effect: _.cloneDeep(nextEffect),
+  });
+
+  return commitPlacementOriginal(nextEffect);
 }
 
 function commitAllHostEffectsReplacement() {
@@ -113,7 +162,7 @@ const reactLibraryPath = './node_modules/react/cjs/react.development.js';
 const reactDOMLibraryPath = './node_modules/react-dom/cjs/react-dom.development.js';
 
 // get replacer method 
-let injectableUseReducer = esprima.parseScript(useReducerReplacement.toString());
+let injectableUseReducer = esprima.parseScript(useReducer.toString());
 let injectableCommitAllHostEffects = esprima.parseScript(
   commitAllHostEffectsReplacement.toString(),
 );
@@ -121,11 +170,10 @@ let injectableCommitAllHostEffects = esprima.parseScript(
 // traverse ast to find method and replace body with our node's body
 function traverseTree(replacementNode, functionName, ast) {
   estraverse.replace(ast, {
-    enter(node) {
-      if (node.type === 'FunctionDeclaration') {
-        if (node.id.name === functionName) {
-          node.body = replacementNode.body[0].body;
-        }
+    enter(node, parent) {
+      if (node.type === 'FunctionDeclaration' && node.id.name === functionName) {
+        parent.body.push(replacementNode.body[0]);
+        node.id.name += 'Original';
       }
     },
   });
@@ -140,10 +188,11 @@ function traverseBundledTree(replacementNode, functionName, ast, library) {
             // create new ast 
             const reactLib = esprima.parseScript(node.value.body.body[1].expression.arguments[0].value);
             estraverse.traverse(reactLib, {
-              enter(libNode) {
+              enter(libNode, parent) {
                 if (libNode.type === 'FunctionDeclaration') {
                   if (libNode.id.name === functionName) {
-                    libNode.body = replacementNode.body[0].body;
+                    parent.body.push(replacementNode.body[0]);
+                    libNode.id.name += 'Original';
                   }
                 }
               },
@@ -158,24 +207,30 @@ function traverseBundledTree(replacementNode, functionName, ast, library) {
 }
 
 const parseAndGenerate = (codeString) => {
-  if (codeString.search('react') !== -1) {
-    const ast = esprima.parseModule(codeString);
-    // Webpack bundle is wrapped in function call
-    if (ast.body[0].expression.type === 'CallExpression') {
-      traverseBundledTree(injectableUseReducer, USEREDUCER, ast, reactLibraryPath);
-      traverseBundledTree(injectableCommitAllHostEffects, COMMITALLHOSTEFFECTS, ast, reactDOMLibraryPath);
-    } else {
-      // parse react-dom code
-      injectableCommitAllHostEffects = esprima.parseScript(commitAllHostEffectsReplacement.toString());
-      traverseTree(injectableCommitAllHostEffects, 'commitAllHostEffects', ast);
+  if (codeString.search('react') === -1) return codeString;
 
-      // parse react code
-      injectableUseReducer = esprima.parseScript(useReducerReplacement.toString());
-      traverseTree(injectableUseReducer, 'useReducer', ast);
-    }
-    const code = escodegen.generate(ast);
-    return code;
+  const injectableCommitWork = esprima.parseScript(commitWork.toString());
+  const injectableCommitDeletion = esprima.parseScript(commitDeletion.toString());
+  const injectableCommitPlacement = esprima.parseScript(commitPlacement.toString());
+  const ast = esprima.parseModule(codeString);
+
+  // Webpack bundle is wrapped in function call
+  if (ast.body[0].expression.type === 'CallExpression') {
+    traverseBundledTree(injectableUseReducer, USEREDUCER, ast, reactLibraryPath);
+    traverseBundledTree(injectableCommitWork, 'commitWork', ast, reactDOMLibraryPath);
+    traverseBundledTree(injectableCommitDeletion, 'commitDeletion', ast, reactDOMLibraryPath);
+    traverseBundledTree(injectableCommitPlacement, 'commitPlacement', ast, reactDOMLibraryPath);
+  } else {
+    // parse react-dom code   
+    traverseTree(injectableCommitWork, 'commitWork', ast);
+    traverseTree(injectableCommitDeletion, 'commitDeletion', ast);
+    traverseTree(injectableCommitPlacement, 'commitPlacement', ast);
+
+    // parse react code
+    injectableUseReducer = esprima.parseScript(useReducer.toString());
+    traverseTree(injectableUseReducer, 'useReducer', ast);
   }
-  return codeString;
+  const code = escodegen.generate(ast);
+  return code;
 };
 module.exports = parseAndGenerate;
